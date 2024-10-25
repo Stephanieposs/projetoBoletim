1da87378640b8bc87fa92d0ddf3c88f4cd0e5495
Rian Gustavo Atanagildo <atanagildo_rian@outlook.com>
2024-10-25 15:56:08 -0300

    feat:ConcertoHidden

--- js/scrip.js
+++ js/scrip.js
@@ -1,33 +1,29 @@
-
 var liDados = document.getElementById('li_dados');
 var liBoletim = document.getElementById('li_boletim');
 var liCertificado = document.getElementById('li_certificado');
 
-var sectionDados = document.getElementById('id_abaDados')
-var sectionBoletim = document.getElementById('id_abaBoletim')
-var sectionCertificado = document.getElementById('id_abaCertificado')
+var sectionDados = document.getElementById('id_abaDados');
+var sectionBoletim = document.getElementById('id_abaBoletim');
+var sectionCertificado = document.getElementById('id_abaCertificado');
 
-
-liDados.addEventListener('click', btDados)
-liBoletim.addEventListener('click', btBoletim)
-liCertificado.addEventListener('click', btCertificado)
+liDados.addEventListener('click', btDados);
+liBoletim.addEventListener('click', btBoletim);
+liCertificado.addEventListener('click', btCertificado);
 
 function btDados() {
-  sectionDados.setAttribute("hidden", "false");
+  sectionDados.removeAttribute("hidden");
   sectionBoletim.setAttribute("hidden", "true");
   sectionCertificado.setAttribute("hidden", "true");
 }
-function btBoletim() {
 
-  sectionDados.setAttribute('hidden', "true");
-  sectionBoletim.setAttribute('hidden', "false");
-  sectionCertificado.setAttribute('hidden', "true");
+function btBoletim() {
+  sectionDados.setAttribute("hidden", "true");
+  sectionBoletim.removeAttribute("hidden");
+  sectionCertificado.setAttribute("hidden", "true");
 }
 
 function btCertificado() {
-
-  sectionDados.setAttribute('hidden', "true");
-  sectionBoletim.setAttribute('hidden', "true");
-  sectionCertificado.setAttribute('hidden', "false");
+  sectionDados.setAttribute("hidden", "true");
+  sectionBoletim.setAttribute("hidden", "true");
+  sectionCertificado.removeAttribute("hidden");
 }
-

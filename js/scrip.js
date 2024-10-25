54bcb7d028e12e524f37fc208370a7a846582302
Stephanie Possamai <stephaniepossamaiamanda@gmail.com>
2024-10-25 15:45:40 -0300

    cols dados

--- js/scrip.js
+++ js/scrip.js
@@ -3,9 +3,9 @@ var liDados = document.getElementById('li_dados');
 var liBoletim = document.getElementById('li_boletim');
 var liCertificado = document.getElementById('li_certificado');
 
-//var sectionDados = document.getElementById('id_abaDados')
-//var sectionBoletim = document.getElementById('id_abaBoletim')
-//var sectionCertificado = document.getElementById('id_abaCertificado')
+var sectionDados = document.getElementById('id_abaDados')
+var sectionBoletim = document.getElementById('id_abaBoletim')
+var sectionCertificado = document.getElementById('id_abaCertificado')
 
 
 liDados.addEventListener('click', btDados)
